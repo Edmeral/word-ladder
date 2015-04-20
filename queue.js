3fbb824d6b8736b419cc6df031bde332fd1f31b1
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-04-20 23:18:54 +0100

    Implementing queue.js

--- queue.js
+++ queue.js
@@ -1,3 +1,15 @@
 function Queue() {
-  
+  this.elements = [];
 }
+
+Queue.prototype = {
+  enqueue: function(elem) {
+    this.elements.push(elem);
+  },
+
+  dequeue: function() {
+    if (this.elements.length === 0)
+      return null;
+    return this.elements.shift();
+  }
+};
